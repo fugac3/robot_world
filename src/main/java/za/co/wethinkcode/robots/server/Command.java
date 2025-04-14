f145fc6a85243155fac5b847f05c7d7b050b6b92
lesego <lephiricpt024@student.wethinkcode.co.za>
2025-04-14 10:54:15 +0200

    test

---
+++ src/main/java/za/co/wethinkcode/robots/server/Command.java
@@ -0,0 +1,12 @@
+package za.co.wethinkcode.robots.server;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Command {
+    @Test
+    void testValidRequests()
+//eeee
+}
