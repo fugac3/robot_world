c6184f9e4ed28399627fac9c4439acba7c8558e9
Hope <k.hope.ing@gmail.com>
2025-04-24 05:19:21 +0200

    Updated tests for RobotTest

--- src/test/java/RobotTest.java
+++ src/test/java/RobotTest.java
@@ -3,65 +3,70 @@ import org.junit.jupiter.api.Test;
 import za.co.wethinkcode.robots.commands.*;
 import za.co.wethinkcode.robots.robot.Position;
 import za.co.wethinkcode.robots.robot.Robot;
+import za.co.wethinkcode.robots.world.TextWorld;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RobotTest {
 
     @Test
-    void initialPosition() {
-        Robot robot = new Robot("CrashTestDummy");
+    void testInitialPosition() {
+        TextWorld world = new TextWorld();
+        Robot robot = new Robot("CrashTestDummy", world);
         assertEquals(Robot.CENTRE, robot.getPosition());
         assertEquals(Direction.NORTH, robot.getCurrentDirection());
+        assertEquals("CrashTestDummy", robot.getName());
+        assertEquals(world, robot.getWorld());
+        assertEquals("Ready", robot.getStatus());
     }
 
     @Test
-    void dump() {
-        Robot robot = new Robot("CrashTestDummy");
-        assertEquals("[0,0] CrashTestDummy> Ready", robot.toString());
-    }
+    void testUpdatePosition() {
+        TextWorld world = new TextWorld();
+        Robot robot = new Robot("CrashTestDummy", world);
 
-    @Test
-    void shutdown() {
-        Robot robot = new Robot("CrashTestDummy");
-        ShutdownCommand command = new ShutdownCommand();
-        assertFalse(robot.handleCommand(command));
-    }
-
-    @Test
-    void forward() {
-        Robot robot = new Robot("CrashTestDummy");
-        ForwardCommand command = new ForwardCommand("10");
-        assertTrue(robot.handleCommand(command));
-        Position expectedPosition = new Position(Robot.CENTRE.getX(), Robot.CENTRE.getY() + 10);
-        assertEquals(expectedPosition, robot.getPosition());
+        boolean result = robot.updatePosition(10);
+        assertTrue(result); //movement should be successful
+        assertEquals(new Position(0, 10), robot.getPosition());
         assertEquals("Moved forward by 10 steps.", robot.getStatus());
     }
 
     @Test
-    void forwardforward() {
-        Robot robot = new Robot("CrashTestDummy");
-        assertTrue(robot.handleCommand(new ForwardCommand("10")));
-        assertTrue(robot.handleCommand(new ForwardCommand("5")));
-        assertEquals("Moved forward by 5 steps.", robot.getStatus());
-    }
+    void testUpdatePositionFail() {
+        TextWorld world = new TextWorld();
+        Robot robot = new Robot("CrashTestDummy", world);
 
-    @Test
-    void tooFarForward() {
-        Robot robot = new Robot("CrashTestDummy");
-        assertTrue(robot.handleCommand(new ForwardCommand("1000")));
-        assertEquals(Robot.CENTRE, robot.getPosition());
+        boolean result = robot.updatePosition(1000);
+        assertFalse(result); //movement should fail/out of bounds
+        assertEquals(new Position(0, 0), robot.getPosition());
         assertEquals("Sorry, I cannot go outside my safe zone.", robot.getStatus());
     }
 
     @Test
-    void help() {
-        Robot robot = new Robot("CrashTestDummy");
-        Command command = new HelpCommand();
-        assertTrue(robot.handleCommand(command));
-        assertEquals("I can understand these commands:\n" +
-                "OFF  - Shut down robot\n" +
-                "HELP - provide information about commands\n" +
-                "FORWARD - move forward by specified number of steps, e.g. 'FORWARD 10'", robot.getStatus());
+    void testTurns() {
+        TextWorld world = new TextWorld();
+        Robot robot = new Robot("CrashTestDummy", world);
+
+        robot.turnLeft();
+        assertEquals(Direction.WEST, robot.getCurrentDirection());
+        robot.turnRight();
+        assertEquals(Direction.NORTH, robot.getCurrentDirection());
+        robot.turnRight();
+        assertEquals(Direction.EAST, robot.getCurrentDirection());
+    }
+
+    @Test
+    void dump() {
+        TextWorld world = new TextWorld();
+        Robot robot = new Robot("CrashTestDummy", world);
+        assertEquals("[0,0] CrashTestDummy> Ready", robot.toString());
+    }
+
+    @Test
+    void quit() { //all robots should disconnect not one!
+        TextWorld world = new TextWorld();
+        Robot robot = new Robot("CrashTestDummy", world);
+        ShutdownCommand command = new ShutdownCommand();
+        assertFalse(robot.handleCommand(command));
     }
 }
