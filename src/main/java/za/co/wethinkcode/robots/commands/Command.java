6dddf4da21c45a768112c80114dde193418b8382
bevnlcpt024 <bevanlcpt024@student.wethinkcode.co.za>
2025-04-29 09:45:25 +0200

    fixed robots

--- src/main/java/za/co/wethinkcode/robots/commands/Command.java
+++ src/main/java/za/co/wethinkcode/robots/commands/Command.java
@@ -36,6 +36,8 @@ public abstract class Command {
         String[] args = instruction.toLowerCase().trim().split(" ");
 
         switch (args[0]){
+            case "robots":
+                return new RobotsCommand();
             case "state":
                 return new StateCommand();
             case "shutdown":
