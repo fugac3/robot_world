aecb21c8bc8e75a7299954732e5e849952d07bb5
benjamin van loggerenberg <bevanlcpt024@student.wethinkcode.co.za>
2025-04-30 00:04:12 +0200

    need to update new commands to match response class

--- src/main/java/za/co/wethinkcode/robots/commands/DumpCommand.java
+++ src/main/java/za/co/wethinkcode/robots/commands/DumpCommand.java
@@ -1,36 +1,39 @@
-package za.co.wethinkcode.robots.commands;
-
-import za.co.wethinkcode.robots.robot.Robot;
-import za.co.wethinkcode.robots.world.IWorld;
-import za.co.wethinkcode.robots.world.Obstacle;
-
-
-import java.util.List;
-
-public class DumpCommand extends Command {
-    public DumpCommand() {
-        super("dump");
-    }
-
-    @Override
-    public boolean execute(Robot target) {
-        System.out.println("=== ROBOT STATE DUMP ===");
-        System.out.println("Robot name: " + target.getName());
-        System.out.println("Position: " + target.getPosition());
-        System.out.println("Direction: " + target.getCurrentDirection());
-        System.out.println("History: " + target.getCommands());
-        System.out.println("Visible surroundings: " + target.lookAround());
-
-        IWorld world = target.getWorld(); // Assuming you can access the world this way
-        if (world != null) {
-            System.out.println("Obstacles in world:");
-            List<Obstacle> obstacles = world.getObstacles();
-            for (Obstacle obs : obstacles) {
-                System.out.println(" - " + obs);
-            }
-        }
-
-        System.out.println("=========================");
-        return true;
-    }
-}
+//package za.co.wethinkcode.robots.commands;
+//
+//import za.co.wethinkcode.robots.robot.Robot;
+//import za.co.wethinkcode.robots.server.Response;
+//import za.co.wethinkcode.robots.world.IWorld;
+//import za.co.wethinkcode.robots.world.Obstacle;
+//
+//
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.stream.Collectors;
+//
+//public class DumpCommand extends Command {
+//    public DumpCommand() {
+//        super("dump");
+//    }
+//
+//    @Override
+//    public Response execute(Robot robot) {
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("robot", robot.getName());
+//        data.put("position", robot.getPosition());
+//        data.put("direction", robot.getCurrentDirection().toString());
+//
+//        IWorld world = robot.getWorld();
+//        if (world != null) {
+//            List<String> obstacleDescriptions = world.getObstacles().stream()
+//                    .map(Obstacle::toString)
+//                    .collect(Collectors.toList());
+//            data.put("obstacles", obstacleDescriptions);
+//        } else {
+//            data.put("obstacles", "World not available.");
+//        }
+//
+//        System.out.println("=========================");
+//        return new Response("OK", "dump complete",data);
+//    }
+//}
