b00c916c99650a98cf0ad29fe421f306a3fb53c2
lesego <lephiricpt024@student.wethinkcode.co.za>
2025-05-02 12:27:20 +0200

    added lookCommand

--- src/main/java/za/co/wethinkcode/robots/commands/LookCommand.java
+++ src/main/java/za/co/wethinkcode/robots/commands/LookCommand.java
@@ -3,14 +3,20 @@ package za.co.wethinkcode.robots.commands;
 import za.co.wethinkcode.robots.robot.Robot;
 import za.co.wethinkcode.robots.server.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LookCommand extends Command {
-    public LookCommand(String name) {
-        super(name);
+    public LookCommand() {
+        super("look");
     }
 
     @Override
     public Response execute(Robot robot) {
-        rob
-        return null;
+        Map<String, String> lookInfo = new HashMap<>();
+        lookInfo.put("Position",robot.getPosition().toString());
+        lookInfo.put("Obstacle", robot.getWorld().getObstacles().toString());
+
+        return new Response("Ok","Robot looking around the world",Map.of(robot.getName(),lookInfo));
     }
 }
