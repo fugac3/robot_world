2cf2170387c788058c591bb3a93f698463caa539
bevnlcpt024 <bevanlcpt024@student.wethinkcode.co.za>
2025-05-07 11:41:46 +0200

    fixed duplicate commands

--- src/main/java/za/co/wethinkcode/robots/commands/Command.java
+++ src/main/java/za/co/wethinkcode/robots/commands/Command.java
@@ -80,11 +80,6 @@ public abstract class Command {
                 return new LookCommand();
             case "state":
                 return new StateCommand();
-            case "fire":
-                return new FireCommand();
-            case "reload":
-                return new ReloadCommand();
-
             default:
                 throw new IllegalArgumentException("Unsupported command: " + instruction);
         }
