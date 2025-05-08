4800b4ce13b30324f29fbeba9a70682aebc87d3a
lesego <lephiricpt024@student.wethinkcode.co.za>
2025-05-08 17:38:35 +0200

    updating test

---
+++ src/test/java/za/co/wethinkcode/SquareObstacleTest.java
@@ -0,0 +1,21 @@
+//package za.co.wethinkcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import za.co.wethinkcode.robots.robot.Position;
+import za.co.wethinkcode.robots.world.SquareObstacle;
+import za.co.wethinkcode.robots.world.TextWorld;
+
+//public class SquareObstacleTest {
+////    private SquareObstacle squareObstacle;
+////    @BeforeEach
+////    public void setUp(){
+////        squareObstacle =
+////
+////    }
+//
+//    @Test
+//    public void getBottomLeftXTest(){
+//        SquareObstacle bottomLeftX = new SquareObstacle();
+//    }
+//}
