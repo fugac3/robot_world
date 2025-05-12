79ed8dc3c8457d44431e79a09275a89c9e00d08c
Hope <k.hope.ing@gmail.com>
2025-05-12 08:16:48 +0200

    Doc strings for forward command

--- src/main/java/za/co/wethinkcode/robots/commands/ForwardCommand.java
+++ src/main/java/za/co/wethinkcode/robots/commands/ForwardCommand.java
@@ -7,15 +7,33 @@ import za.co.wethinkcode.robots.server.Response;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Command to move the robot forward.
+ * This command moves the robot forward by the specified number of steps in the direction it is facing.
+ */
 public class ForwardCommand extends Command {
 
     private final String argument;
 
+    /**
+     * Creates a new ForwardCommand instance.
+     *
+     * @param argument The number of steps to move forward
+     */
     public ForwardCommand(String argument) {
         super("forward", argument);
         this.argument = argument;
     }
 
+    /**
+     * Executes the forward command, moving the robot forward by the specified number of steps.
+     *
+     * @param robot The robot to move forward
+     * @return A response indicating success or failure:
+     *         - OK if the robot moved successfully
+     *         - FAILED if the robot was obstructed or reached the edge of the world
+     *         - ERROR if the steps argument is not a valid number
+     */
     @Override
     public Response execute(Robot robot) {
         boolean moved;
@@ -41,6 +59,11 @@ public class ForwardCommand extends Command {
         }
     }
 
+    /**
+     * Returns a string representation of this command.
+     *
+     * @return A string describing this command
+     */
     @Override
     public String toString() {
         // This will be used for logging the command
