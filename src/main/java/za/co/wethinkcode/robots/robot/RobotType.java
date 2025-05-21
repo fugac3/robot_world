ee2ac423b5bc1ad238e89fbd4c761970aa55bb0c
Hope <k.hope.ing@gmail.com>
2025-05-21 15:05:55 +0200

    Created getters for RobotType

--- src/main/java/za/co/wethinkcode/robots/robot/RobotType.java
+++ src/main/java/za/co/wethinkcode/robots/robot/RobotType.java
@@ -2,15 +2,31 @@ package za.co.wethinkcode.robots.robot;
 
 public class RobotType {
 
-    private String typeName;
-    private int maxShieldStrength;
-    private int maxShots;
-    private int shootingRange;
+    private final String typeName; //type of robot
+    private final int maxShieldStrength; //number of hits shield can take
+    private final int maxShots; //number of shots before reloading
+    private final int shootingRange; //distance that can be shot
 
     public RobotType(String typeName, int maxShieldStrength, int maxShots, int shootingRange){
         this.typeName = typeName;
         this.maxShieldStrength = maxShieldStrength;
         this.maxShots = maxShots;
         this.shootingRange = shootingRange;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public int getMaxShieldStrength() {
+        return maxShieldStrength;
+    }
+
+    public int getMaxShots() {
+        return maxShots;
+    }
+
+    public int getShootingRange() {
+        return shootingRange;
     }
 }
