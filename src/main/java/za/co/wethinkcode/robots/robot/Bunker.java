d3bd8a858262fb87ff1eef9563421740dbbc406d
Hope <k.hope.ing@gmail.com>
2025-05-21 17:16:56 +0200

    Created Bunker robot

---
+++ src/main/java/za/co/wethinkcode/robots/robot/Bunker.java
@@ -0,0 +1,8 @@
+package za.co.wethinkcode.robots.robot;
+
+public class Bunker extends RobotType{ //robot with good defense, inspired by war bunkers
+
+    public Bunker() {
+        super("Bunker", 5, 2, 1);
+    }
+}
