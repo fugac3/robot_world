082f222d0820a2b0d77e9ec4fa6afb97b28677d2
Hope <k.hope.ing@gmail.com>
2025-05-22 09:33:29 +0200

    Created Wasp robot and changed file names

--- src/main/java/za/co/wethinkcode/robots/robot/Cannon.java
+++
@@ -1,8 +0,0 @@
-package za.co.wethinkcode.robots.robot;
-
-public class Cannon extends RobotType{ //robot with long shooting range and huge blast
-
-    public Cannon() {
-        super("Cannon", 3, 2, 5);
-    }
-}
---
+++ src/main/java/za/co/wethinkcode/robots/robot/CannonRobot.java
@@ -0,0 +1,8 @@
+package za.co.wethinkcode.robots.robot;
+
+public class CannonRobot extends RobotType{ //robot with long shooting range and huge blast
+
+    public CannonRobot() {
+        super("Cannon", 3, 2, 5);
+    }
+}
