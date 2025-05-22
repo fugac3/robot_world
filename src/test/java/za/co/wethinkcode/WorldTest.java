1b60abce427c127fb2fbdb733c10e62c80537dcd
lesego <lephiricpt024@student.wethinkcode.co.za>
2025-05-22 13:36:34 +0200

    test

--- src/test/java/za/co/wethinkcode/WorldTest.java
+++ src/test/java/za/co/wethinkcode/WorldTest.java
@@ -1,5 +1,6 @@
 package za.co.wethinkcode;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +12,8 @@ import za.co.wethinkcode.robots.robot.Robot;
 import java.util.List;
 
 public class WorldTest {
+    private TextWorld textWorld;
+
     @Test
     void testWorldCreation() { //world should start off with at least one obstacle
         TextWorld world = new TextWorld();
@@ -72,4 +75,56 @@ public class WorldTest {
         Position outsideLeft = new Position(-201, 0);
         assertFalse(outsideLeft.isIn(topLeft, bottomRight));
     }
+
+
+
+        @BeforeEach
+        public void setUp() {
+            textWorld = TextWorld.getInstance(); // Get singleton instance
+        }
+
+        @Test
+        public void testSingletonInstance() {
+            TextWorld anotherInstance = TextWorld.getInstance();
+            assertSame(textWorld, anotherInstance, "TextWorld should return the same instance each time");
+        }
+
+        @Test
+        public void testAddRobot() {
+            // Create a new robot
+            Position pos = new Position(0,0);
+            Robot robot = new Robot("bot", textWorld,pos);
+
+            // Add the robot to the world
+            textWorld.addRobot(robot);
+
+            // Verify that the robot was added successfully
+            assertTrue(textWorld.getAllRobots().contains(robot), "Robot should be added to the world");
+        }
+
+        @Test
+        public void testgetAllRobots(){
+            Position pos = new Position(0,0);
+            Robot robot = new Robot("bot", textWorld,pos);
+
+            textWorld.getAllRobots();
+
+            assertEquals(textWorld.getAllRobots() , textWorld.getAllRobots());
+        }
+
+        @Test
+        public void testBlocksPath() {
+            // Create a new world and a robot
+            Position pos = new Position(4,5);
+            Robot robot = new Robot("bot", textWorld,pos);
+
+            // Set up some obstacles (this should be done in your world setup)
+            textWorld.blocksPath(new Position(5, 5), new Position(9, 9)); // Example, actual obstacle setup needed
+
+            // Test if the path between two positions is blocked
+            boolean isPathBlocked = textWorld.blocksPath(new Position(4, 5), new Position(10, 5));
+
+            // Assert that the path is blocked (modify as per actual obstacle setup)
+            assertTrue(isPathBlocked);
+        }
 }
