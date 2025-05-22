f80f431113660c0abcb4d4f6932ddc57a99967e7
Hope <k.hope.ing@gmail.com>
2025-05-22 09:38:34 +0200

    Created Stormcaller robot

---
+++ src/main/java/za/co/wethinkcode/robots/robot/StormcallerRobot.java
@@ -0,0 +1,8 @@
+package za.co.wethinkcode.robots.robot;
+
+public class StormcallerRobot extends RobotType{ //robot that fires lightning bolts
+
+    public StormcallerRobot(){
+        super("Stormcaller", 2, 4, 5);
+    }
+}
