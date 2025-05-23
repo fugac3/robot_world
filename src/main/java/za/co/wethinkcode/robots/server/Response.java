1d89925b12fb8681041bcf9a4bed64e85ec1b0ec
lesego <lephiricpt024@student.wethinkcode.co.za>
2025-05-23 17:28:51 +0200

    add the robot health

--- src/main/java/za/co/wethinkcode/robots/server/Response.java
+++ src/main/java/za/co/wethinkcode/robots/server/Response.java
@@ -11,6 +11,8 @@ public class Response {
     private final Map<String, Object> data;
     private final Map<String, Object> state;
 
+
+
     public Response(String result, Map<String, Object> data, Robot robot) {
         this.result = result;
         this.data = data;
@@ -25,6 +27,7 @@ public class Response {
         currentState.put("shots", robot.getAmmo());
         currentState.put("direction", robot.getCurrentDirection());
         currentState.put("status", robot.getStatus());
+        currentState.put("Health", robot.getCurrentShieldStrength());
         return currentState;
     }
 
@@ -39,6 +42,9 @@ public class Response {
     public String getResult() {
         return result;
     }
+
+
+
 
 
 }
