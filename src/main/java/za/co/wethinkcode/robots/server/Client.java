20347efa8e602deb37ed8433d1bba218a9df66d6
lesego <lephiricpt024@student.wethinkcode.co.za>
2025-05-27 17:28:02 +0200

    read me

--- src/main/java/za/co/wethinkcode/robots/server/Client.java
+++ src/main/java/za/co/wethinkcode/robots/server/Client.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
 
-        int Port = 4401;
+        int Port = 4402;
 
         Socket socket = null;
 //        InputStreamReader inputStreamReader = null;  //byte based
